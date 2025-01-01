a19c058eb6b2cf696b1f750fd8034832ca94c7ee
Dat Le <datle1108@gmail.com>
2025-01-01 10:49:16 +0100

    Add events page with heading

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,24 @@
-export default function Events() {
-  return <div>event page</div>;
+import H1 from "@/components/H1";
+
+type EventsProps = {
+  params: {
+    city: string;
+  };
+};
+
+export default function Events({ params }: EventsProps) {
+  const { city } = params;
+
+  return (
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>
+        {city === "all" && "All Events"}
+        {city !== "all" && (
+          <span>
+            Events in <span className="capitalize">{city}</span>
+          </span>
+        )}
+      </H1>
+    </main>
+  );
 }
