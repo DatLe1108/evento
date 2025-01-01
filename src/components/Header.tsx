6e57bbfe2333bf9b4eb881e104d29ca5e612d8dc
Dat Le <datle1108@gmail.com>
2025-01-01 10:04:03 +0100

    Active link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   {
@@ -13,17 +18,31 @@ const routes = [
 ];
 
 export default function Header() {
+  const activePathname = usePathname();
+
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 sm:px-9 px-3">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="flex gap-x-6 text-sm h-full">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white transition"
+              className={clsx(
+                "hover:text-white transition relative flex items-center",
+                {
+                  "text-white": activePathname === route.path,
+                  "text-white/50": activePathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                />
+              )}
             </li>
           ))}
         </ul>
